ed8e724843a1943a3cf17e0373de83f47a6a971f
Yousefgareap <yousefgareap@gmail.com>
2023-08-11 04:23:07 -0700

    new

--- webpack.config.js
+++ webpack.config.js
@@ -89,6 +89,10 @@ module.exports = {
       template: "./src/payment.html",
       filename: "payment.html",
     }),
+    new HtmlWebpackPlugin({
+      template: "./src/search.html",
+      filename: "search.html",
+    }),
     new MiniCssExtractPlugin({
       filename: "css/style.css",
     }),
